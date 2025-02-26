0128c47c68fc9b0c50871ffb797129f7c6820ff1
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-02-26 18:31:53 -0300

    feat: add React Query Devtools

--- src/contexts/AppProvider.tsx
+++ src/contexts/AppProvider.tsx
@@ -4,9 +4,13 @@ import type { ReactNode } from "react";
 
 import { queryClient } from "../services/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function AppProvider({ children }: { children: ReactNode }) {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
